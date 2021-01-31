b3bcd66acd1c12a440869b17afeff68b0135d321
Abiyot Abayneh <abey2get@yahoo.co.uk>
2021-01-31 16:05:53 +0000

    show cloud

--- app.js
+++ app.js
@@ -115,7 +115,8 @@ app.use(
                 "'self'",
                 "blob:",
                 "data:",
-                "https://res.cloudinary.com/douqbebwk/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                // "https://res.cloudinary.com/douqbebwk/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://res.cloudinary.com/console/c-d4d100ed6d7dc44d4d6cd67fcc354a",
                 "https://images.unsplash.com/",
             ],
             fontSrc: ["'self'", ...fontSrcUrls],
