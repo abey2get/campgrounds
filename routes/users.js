7ac4539c7170057abfe37f07f8aa96e250d7778a
Abiyot Abayneh <abey2get@yahoo.co.uk>
2021-02-11 13:23:40 +0000

    no register

--- routes/users.js
+++ routes/users.js
@@ -5,9 +5,9 @@ const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
 const users = require('../controllers/users');
 
-router.route('/register')
-    .get(users.renderRegister)
-    .post(catchAsync(users.register));
+// router.route('/register')
+//     .get(users.renderRegister)
+//     .post(catchAsync(users.register));
 
 router.route('/login')
     .get(users.renderLogin)
